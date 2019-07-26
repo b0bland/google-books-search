df0667706140eacce73e199fe6994aa8155aee7f
b0bland <alfredychan96@gmail.com>
2019-07-26 11:15:23 -0700

    Updates

---
+++ precache-manifest.4947cdf943bb80d82785366c558eaa86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429d88c47c94a0ff6af815422bc3b53a",
+    "url": "/google-books-search/index.html"
+  },
+  {
+    "revision": "8288e06e9aca0b813fd7",
+    "url": "/google-books-search/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/google-books-search/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8288e06e9aca0b813fd7",
+    "url": "/google-books-search/static/js/main.eecd6ccb.chunk.js"
+  },
+  {
+    "revision": "d672f040abfa3c0ea28a",
+    "url": "/google-books-search/static/js/runtime~main.9ad78463.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
